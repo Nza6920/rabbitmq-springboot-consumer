c4ed01fc1a69c2140c47dddd22fe90095aba3b6d
NiuZian <zian.niu@gdcccn.com>
2020-07-08 16:57:35 +0800

    init project

---
+++ src/main/java/com/niu/springboot/RabbitmqSpringbootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.niu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringbootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSpringbootConsumerApplication.class, args);
+    }
+
+}
