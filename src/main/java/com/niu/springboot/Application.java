bd777fec9ec5b1a42906d87c58e3c6bb48d0dbc4
Nza6920 <1484663282@qq.com>
2020-07-08 22:16:04 +0800

    add rabbitmq config

--- src/main/java/com/niu/springboot/Application.java
+++ src/main/java/com/niu/springboot/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqSpringbootConsumerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitmqSpringbootConsumerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
